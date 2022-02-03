eb561cecc886fa4590bc5178d8f4d6f7a6009d6e
gonzjv <gonzjv@ya.ru>
2022-02-03 11:38:20 +0300

    refactor: header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 class Header extends React.Component {
   render() {
-    return <header>Header in da house</header>;
+    return (
+      <header>
+        <nav className="navigation">
+          <Link to="tech">Tech</Link>
+          <Link to="clothes">Clothes</Link>
+        </nav>
+      </header>
+    );
   }
 }
+
+export default Header;
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from '../header/header.js';
 import './layout.css';
 
 class Layout extends React.Component {
   render() {
     return (
       <div className="layout">
-        <nav className="navigation">
-          <Link to="tech">Tech</Link>
-          <Link to="clothes">Clothes</Link>
-        </nav>
-        <main>
-          <Outlet />
-        </main>
+        <Header />
+        <Outlet />
       </div>
     );
   }
